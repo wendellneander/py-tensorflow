788f386b81488d1296b5c07963c2b3842b869760
Wendell Neander <wendell.neander@mirumagency.com>
2019-11-19 14:18:18 -0300

    format code

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ import numpy as np
 from tensorflow import keras
 
 
-model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model = keras.Sequential([
+    keras.layers.Dense(units=1, input_shape=[1])
+])
+
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
