1c1b6c0869bf15ae08c888b1355705470373e430
Wendell Neander <wendell.neander@mirumagency.com>
2019-11-20 18:36:38 -0300

    example2

--- app/main.py
+++ app/main.py
@@ -1,16 +1,44 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 
 
-model = keras.Sequential([
-    keras.layers.Dense(units=1, input_shape=[1])
-])
+def example_1():
+    model = keras.Sequential([
+        keras.layers.Dense(units=1, input_shape=[1])
+    ])
 
-model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+    xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+    ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
-model.fit(xs, ys, epochs=500)
+    model.fit(xs, ys, epochs=500)
 
-print(model.predict([10.0]))
+    predict = model.predict([10.0])
+    print(predict)
+
+    return predict
+
+
+def example_2():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=tf.nn.relu),
+        keras.layers.Dense(128, activation=tf.nn.softmax),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer=tf.train.AdamOptimizer(), loss='sparse_categorical_crossentropy')
+
+    model.fit(train_images, train_labels, epochs=5)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    my_images = []
+
+    predictions = model.predict(my_images)
+
+
